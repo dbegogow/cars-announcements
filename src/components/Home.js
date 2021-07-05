e16dba102e46ee29bccea073e4a9f8425e5e0696
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-05 22:43:54 +0300

    Implement FilterContainer structure and styles in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,16 @@ const Home = () => {
     return (
         <Wrapper>
             <FiltersContainer>
-                Filters
+                <Title>Филтрирай</Title>
+                <FilterButton>Купе</FilterButton>
+                <FilterButton>Марка</FilterButton>
+                <FilterButton>Гориво</FilterButton>
+                <FilterButton>Скорости</FilterButton>
+                <FilterButton>Цена</FilterButton>
+                <FilterButton>Година</FilterButton>
+                <FilterButton>Брой врати</FilterButton>
+                <FilterButton>Мощност</FilterButton>
+                <SerchButton>Търси</SerchButton>
             </FiltersContainer>
             <CarsContainer>
                 <CarCard />
@@ -36,10 +45,10 @@ const Wrapper = styled.div`
 
 const FiltersContainer = styled.div`
     position: fixed;
-    background: wheat;
     width: 20vw;
-    height: 80vh;
+    height: 60vh;
     margin-right: 8vw;
+    border: 2px solid #585858;
 `;
 
 
@@ -52,4 +61,47 @@ const CarsContainer = styled.div`
     grid-template-columns: repeat(3, 1fr);
     column-gap: 20px;
     row-gap: 50px;
+`;
+
+const Title = styled.h3`
+    text-align: center;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 1.3rem;
+`;
+
+const FilterButton = styled.button`
+    width: 12vw;
+    margin-left: 3.5vw;
+    margin-bottom: 10px;
+    padding: 7px 0;
+    border-radius: 15px;
+    border: none;
+    background: #3E98C3;
+    border: 1px solid #3E98C3;
+    color: #FFF;
+    font-size: 0.95rem;
+
+    &:hover {
+        cursor: pointer;
+        background: #8CF1F5;
+        color: #000;
+        border: 1px solid #000;
+    }
+`;
+
+const SerchButton = styled.button`
+    width: 15vw;
+    margin-left: 2vw;
+    margin-top: 3px;
+    border-radius: 15px;
+    border: 2px solid #1423AB;
+    background: #2FA3F0;
+    padding: 10px 0;
+    color: #FFF;
+    font-size: 0.95rem;
+
+    &:hover {
+        cursor: pointer;
+        background: #03B8F0;
+    }
 `;
