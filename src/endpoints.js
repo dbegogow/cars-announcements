16365fa38b57390d38cbca4c62e285486675f7e6
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-15 22:35:55 +0300

    Implement service for filter data

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.REACT_APP_API_URL;
+export const baseUrl = process.env.REACT_APP_API_URL;
 
 export const urlCars = `${baseUrl}/cars`;
--- src/services/cars.js
+++ src/services/cars.js
@@ -1,7 +1,13 @@
-import { urlCars } from '../endpoints';
+import { baseUrl, urlCars } from '../endpoints';
 
 export const allCars = async () => {
     return fetch(urlCars)
+        .then(res => res.json())
+        .catch(err => console.log(err.message));
+};
+
+export const filterData = async (filter) => {
+    return fetch(`${baseUrl}/${filter}`)
         .then(res => res.json())
         .catch(err => console.log(err.message));
 };
