58025e0642e45b985636df372d62f543b4926774
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-29 23:20:35 +0300

    Clear logic in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,65 +6,12 @@ import Filters from './Filters';
 import Overlay from './Overlay';
 
 const Home = () => {
-    const [isOpenFilter, setIsOpenFilter] = useState(false);
-    const [isUserData, setIsUserData] = useState(false);
-    const [modalHeight, setModalHeight] = useState();
-    const [modalTop, setModalTop] = useState();
-
-    const [model, setModel] = useState();
-    const [brand, setBrand] = useState();
-    const [fuel, setFuel] = useState();
-    const [transmission, setTransmission] = useState();
-    const [fromPrice, setFromPrice] = useState(0);
-    const [toPrice, setToPrice] = useState(0);
-    const [fromYear, setFromYear] = useState(0);
-    const [toYear, setToYear] = useState(0);
-    const [doors, setDoors] = useState(0);
-    const [fromPower, setFromPower] = useState(0);
-    const [toPower, setToPower] = useState(0);
-
-    const openFilter = async (e) => {
-        const target = e.target;
-
-        if (target.localName !== 'button') { return; }
-
-        var attribute = target.getAttribute('data');
-
-        if (attribute === 'price' ||
-            attribute === 'year' ||
-            attribute === 'power') {
-            setModalHeight('200px');
-            setModalTop('50%');
-            setIsUserData(true);
-        } else {
-            setModalHeight('350px');
-            setModalTop('40%');
-            setIsUserData(false)
-        }
-
-        setIsOpenFilter(true);
-    };
-
-    const closeFilter = () => {
-        setIsOpenFilter(false);
-    };
 
     return (
         <Wrapper>
-            <Filters openFilter={openFilter} />
+            <Filters />
             <Overlay />
             <CarsContainer>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
                 <Card />
             </CarsContainer>
         </Wrapper>
