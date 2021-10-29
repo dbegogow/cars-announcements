b74e634fe09003861a701015a939c21b821d9279
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-29 23:19:20 +0300

    Export overlay in component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import { filterData } from '../services/cars';
 import Card from './Card';
 import Filters from './Filters';
+import Overlay from './Overlay';
 
 const Home = () => {
     const [isOpenFilter, setIsOpenFilter] = useState(false);
-    const [userData, setUserData] = useState(null);
-    const [serverData, setServerData] = useState([]);
+    const [isUserData, setIsUserData] = useState(false);
     const [modalHeight, setModalHeight] = useState();
     const [modalTop, setModalTop] = useState();
 
@@ -33,36 +33,13 @@ const Home = () => {
         if (attribute === 'price' ||
             attribute === 'year' ||
             attribute === 'power') {
-            setUserData(target.textContent);
             setModalHeight('200px');
             setModalTop('50%');
+            setIsUserData(true);
         } else {
-            setUserData(null);
             setModalHeight('350px');
             setModalTop('40%');
-        }
-
-        switch (attribute) {
-            case 'model':
-                await filterData('models')
-                    .then(data => setServerData({ name: data, filter: 'model' }));
-                break;
-            case 'brand':
-                await filterData('brands')
-                    .then(data => setServerData({ name: data, filter: 'model' }));
-                break;
-            case 'fuel':
-                await filterData('fuels')
-                    .then(data => setServerData({ name: data, filter: 'model' }));
-                break;
-            case 'transmission':
-                await filterData('transmissions')
-                    .then(data => setServerData({ name: data, filter: 'model' }));
-                break;
-            case 'doors':
-                await filterData('doors')
-                    .then(data => setServerData({ name: data, filter: 'model' }));
-                break;
+            setIsUserData(false)
         }
 
         setIsOpenFilter(true);
@@ -75,32 +52,7 @@ const Home = () => {
     return (
         <Wrapper>
             <Filters openFilter={openFilter} />
-            <div style={{ display: isOpenFilter ? '' : 'none' }}>
-                <Overlay />
-                <Modal height={modalHeight} top={modalTop}>
-                    <CloseFilterButton onClick={closeFilter}>
-                        <i className="fas fa-times"></i>
-                    </CloseFilterButton>
-                    <ServerData style={{ display: userData ? 'none' : '' }}>
-                        {
-                            serverData?.map(data => <Item>{data.name}</Item>)
-                        }
-                    </ServerData>
-                    <UserData style={{ display: userData ? '' : 'none' }}>
-                        <h2>{userData}</h2>
-                        <div>
-                            <form>
-                                <label htmlFor="from">От:</label>
-                                <input id="from" type="text" />
-                                <label htmlFor="to">До:</label>
-                                <input id="to" type="text" />
-                                <button>Филтрирай</button>
-                            </form>
-                            <button>Изчисти</button>
-                        </div>
-                    </UserData>
-                </Modal>
-            </div>
+            <Overlay />
             <CarsContainer>
                 <Card />
                 <Card />
@@ -144,124 +96,5 @@ const CarsContainer = styled.div`
 
     @media (max-width: 890px) {
         grid-template-columns: repeat(1, 1fr);
-    }
-`;
-
-const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background-color: rgba(0,0,0,0.5);
-    z-index: 10;
-`;
-
-const Modal = styled.div.attrs(props => ({
-    height: props.height,
-    top: props.top
-}))`
-    width: 600px;
-    height: ${props => props.height};
-    line-height: 200px;
-    position: fixed;
-    top: ${props => props.top}; 
-    left: 50%;
-    margin-top: -100px;
-    margin-left: -300px;
-    background-color: #22A1E2;
-    border-radius: 5px;
-    z-index: 11;
-
-    @media (max-width: 680px) {
-        width: 300px;
-        margin-left: -150px;
-    }
-`;
-
-const CloseFilterButton = styled.button`
-    position: absolute;
-    top: 10px;
-    right: 15px;
-    padding: 0;
-    color: #BF2B11;
-    background: none;
-    border: none;
-    font-size: 1.2rem;
-
-    &:hover {
-        cursor: pointer;
-    }
-`;
-
-const ServerData = styled.div`
-    margin: 40px 10px 15px 10px;
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-
-    @media (max-width: 680px) {
-        grid-template-columns: repeat(3, 1fr);
-    }
-`;
-
-const Item = styled.button`
-    background: #FFF;
-    margin: 7px;
-    border: 2px solid #6C6C6C;
-    border-radius: 12px;
-    padding: 5px;
-    font-size: 0.9rem;
-
-    &:hover {
-        cursor: pointer;
-        background: #E0E0E0;
-        transition: 0.4s ease;
-    }
-`;
-
-const UserData = styled.div`
-    margin-left: 40px;
-    margin-top: 10px;
-    color: #FFF;
-
-    h2 {
-        margin: 0;
-        position: absolute;
-        bottom: 45px;
-        font-size: 1.5rem;
-        color: #FFF;
-    }
-
-    form {
-        display: inline-block;
-    }
-
-    div label {
-        margin-right: 3px;
-        font-size: 1.2rem;
-    }
-
-    div input {
-        margin-right: 20px;
-        width: 100px;
-        outline: none;
-        border: 1px solid #B6B6B6;
-        border-radius: 12px;
-        padding: 5px 10px;
-    }
-
-    button {
-        border: 1px solid #B6B6B6;
-        border-radius: 8px;
-        padding: 5px 10px;
-        font-size: 0.9rem;
-        background: #E0E0E0;
-        color: #000;
-        margin-right: 10px;
-
-        &:hover {
-            cursor: pointer;
-            transform: scale(1.04);
-        }
     }
 `;
---
+++ src/components/Overlay.js
@@ -0,0 +1,154 @@
+import styled from 'styled-components';
+
+const Overlay = () => {
+    const isUserData = false;
+
+    return (
+        <div>
+            <Container />
+            <Modal height='200px' top='50%'>
+                <CloseFilterButton>
+                    <i className="fas fa-times"></i>
+                </CloseFilterButton>
+                <ServerData style={{ display: isUserData ? 'none' : '' }}>
+                    <Item>Mercedes</Item>
+                    <Item>Audi</Item>
+                </ServerData>
+                <UserData style={{ display: isUserData ? '' : 'none' }}>
+                    <h2></h2>
+                    <div>
+                        <form>
+                            <label htmlFor="from">От:</label>
+                            <input id="from" type="text" />
+                            <label htmlFor="to">До:</label>
+                            <input id="to" type="text" />
+                            <button>Филтрирай</button>
+                        </form>
+                        <button>Изчисти</button>
+                    </div>
+                </UserData>
+            </Modal>
+        </div>
+    );
+};
+
+export default Overlay;
+
+const Container = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 10;
+`;
+
+const Modal = styled.div.attrs(props => ({
+    height: props.height,
+    top: props.top
+}))`
+    width: 600px;
+    height: ${props => props.height};
+    line-height: 200px;
+    position: fixed;
+    top: ${props => props.top}; 
+    left: 50%;
+    margin-top: -100px;
+    margin-left: -300px;
+    background-color: #22A1E2;
+    border-radius: 5px;
+    z-index: 11;
+
+    @media (max-width: 680px) {
+        width: 300px;
+        margin-left: -150px;
+    }
+`;
+
+const CloseFilterButton = styled.button`
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    padding: 0;
+    color: #BF2B11;
+    background: none;
+    border: none;
+    font-size: 1.2rem;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const ServerData = styled.div`
+    margin: 40px 10px 15px 10px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+
+    @media (max-width: 680px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+`;
+
+const Item = styled.button`
+    background: #FFF;
+    margin: 7px;
+    border: 2px solid #6C6C6C;
+    border-radius: 12px;
+    padding: 5px;
+    font-size: 0.9rem;
+
+    &:hover {
+        cursor: pointer;
+        background: #E0E0E0;
+        transition: 0.4s ease;
+    }
+`;
+
+const UserData = styled.div`
+    margin-left: 40px;
+    margin-top: 10px;
+    color: #FFF;
+
+    h2 {
+        margin: 0;
+        position: absolute;
+        bottom: 45px;
+        font-size: 1.5rem;
+        color: #FFF;
+    }
+
+    form {
+        display: inline-block;
+    }
+
+    div label {
+        margin-right: 3px;
+        font-size: 1.2rem;
+    }
+
+    div input {
+        margin-right: 20px;
+        width: 100px;
+        outline: none;
+        border: 1px solid #B6B6B6;
+        border-radius: 12px;
+        padding: 5px 10px;
+    }
+
+    button {
+        border: 1px solid #B6B6B6;
+        border-radius: 8px;
+        padding: 5px 10px;
+        font-size: 0.9rem;
+        background: #E0E0E0;
+        color: #000;
+        margin-right: 10px;
+
+        &:hover {
+            cursor: pointer;
+            transform: scale(1.04);
+        }
+    }
+`;
